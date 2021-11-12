35e364e8c48b67b8311d2be019dbb6713e37a8a2
Tim Whitbeck <tim.whitbeck@stord.com>
2021-11-12 16:10:44 -0500

    use KV for url slugs

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,8 +1,32 @@
 import generateRandomSlug from "./generate-random-slug";
 
 const handler: ExportedHandler<{ URLS: KVNamespace }> = {
-  fetch(request, { URLS }, ctx) {
+  async fetch(request, { URLS }, ctx) {
+    if (request.method !== "POST") {
+      return new Response(null, { status: 405 });
+    }
+
+    const body = await request.text();
+
+    if (!body) {
+      return new Response(null, { status: 400 });
+    }
+
+    try {
+      new URL(body);
+    } catch (error) {
+      return new Response(null, { status: 400 });
+    }
+
+    const existingSlug = await URLS.get(body);
+
+    if (existingSlug) {
+      return new Response(existingSlug);
+    }
+
     const slug = generateRandomSlug();
+
+    await URLS.put(body, slug);
 
     return new Response(slug);
   },
